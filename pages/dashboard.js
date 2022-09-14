a153879771149f617ae7c0bdfd31b4388ff6d615
Isma Rianto <ismarianto97@gmail.com>
2022-09-14 20:21:40 +0700

    first commit

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -136,8 +136,7 @@ const Dashboard = () => {
                             <Card.Body>
                                 <Card.Title>{variant} Card Title </Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
+                                   Test
                                 </Card.Text>
                             </Card.Body>
                         </Card>
